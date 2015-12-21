c9cd3202b2267c5833380b2ff95c5deac024e17f
Christopher Aue <mail@christopheraue.net>
2015-12-21 16:58:20 +0100

    only report keyboard events

--- examples/input_device_events.rb
+++ examples/input_device_events.rb
@@ -7,9 +7,9 @@ root_window = display.screens.first.root_window
 top_level_windows = root_window.property(:_NET_CLIENT_LIST_STACKING)
 
 top_level_windows.each do |window|
-  %i(xi_key_press xi_button_press xi_motion xi_enter xi_leave).each do |name, _|
+  %i(xi_key_press xi_key_release xi_focus_in xi_focus_out).each do |name, _|
     window.on(name, name) do |event|
-      puts "#{event.event}: #{XlibObj::Extension::XI::Event::TYPES.key(event.evtype)}, device #{event.deviceid}"
+      puts "#{event.event}: #{XlibObj::Extension::XI::Event::TYPES.key(event.evtype)}, device #{event.deviceid}, source #{event.sourceid}"
     end
   end
 end
@@ -17,4 +17,3 @@ end
 loop do
   display.handle_events
 end
-
