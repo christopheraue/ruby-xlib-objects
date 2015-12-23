9bf98982462314430cfb87f04fcb91879d897b08
Christopher Aue <mail@christopheraue.net>
2015-12-23 17:07:51 +0100

    flushed after device grab

--- lib/xlib/xi.rb
+++ lib/xlib/xi.rb
@@ -40,7 +40,9 @@ module Xlib
       def grab_device(display, device, window, time, cursor, mode, pair_mode, owner_events, event_mask)
         event_mask = EventMask.new(device, event_mask)
         0 == Xlib::XIGrabDevice(display.to_native, device.to_native, window.to_native, time, cursor,
-          mode, pair_mode, owner_events, event_mask.to_native)
+          mode, pair_mode, owner_events, event_mask.to_native).tap do
+          Xlib::X.flush(display)
+        end
       end
 
       def ungrab_device(display, device, time)
